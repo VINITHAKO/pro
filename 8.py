b302553e82474f1a44790f7921dec67c28fb5832
VINITHAKO <51981011+VINITHAKO@users.noreply.github.com>
2019-07-26 14:47:43 +0530

    Update 8.py

--- 8.py
+++ 8.py
@@ -3,8 +3,8 @@ v3,k3=map(int,input().split())
 de1=[]
 bb1=list(map(int,input().split()))
 for i in range(0,k3):
-    l1,s1=map(int,input().split())
-    sp1.append([l1,s1])
+    l1,h1=map(int,input().split())
+    sp1.append([l1,h1])
 for i in sp1:
     ee1=i[0]-1
     mm1=i[1]-1
