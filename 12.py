34b20ab1f045eaec600b80c24483fc17b61d631b
VINITHAKO <51981011+VINITHAKO@users.noreply.github.com>
2019-07-26 15:04:26 +0530

    Update 12.py

--- 12.py
+++ 12.py
@@ -1,12 +1,12 @@
 #a
-k,m=map(int,input().split())
-mk=list(map(int,input().split()))
-l1=[]
-for i in range(0,m):
-     a,b=map(int,input().split())
-     l1.append([a,b])
-for i in range(m):
-     lower=l1[i][0]
-     upper=l1[i][1]
-     x=sum(mk[lower-1:upper])
+v,i=map(int,input().split())
+iv=list(map(int,input().split()))
+ss=[]
+for i in range(0,i):
+     c,d=map(int,input().split())
+     ss.append([c,d])
+for i in range(i):
+     lower=[i][0]
+     upper=ss[i][1]
+     x=sum(iv[lower-1:upper])
      print(x)
