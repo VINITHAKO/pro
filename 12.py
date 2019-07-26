8ef4aeda0780883ac39b32002c9fee1c92457d1a
VINITHAKO <51981011+VINITHAKO@users.noreply.github.com>
2019-07-26 15:07:57 +0530

    Update 12.py

--- 12.py
+++ 12.py
@@ -1,12 +1,12 @@
 #a
-v,i=map(int,input().split())
+v,a=map(int,input().split())
 iv=list(map(int,input().split()))
 ss=[]
-for i in range(0,i):
+for i in range(0,a):
      c,d=map(int,input().split())
      ss.append([c,d])
-for i in range(i):
-     lower=[i][0]
+for i in range(a):
+     lower=ss[i][0]
      upper=ss[i][1]
      x=sum(iv[lower-1:upper])
      print(x)
