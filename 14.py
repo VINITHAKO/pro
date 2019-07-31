87d7a6698abfb093cc524b1d91f85c5e15cd507a
VINITHAKO <51981011+VINITHAKO@users.noreply.github.com>
2019-07-31 11:08:11 +0530

    Create 14.py

---
+++ 14.py
@@ -0,0 +1,14 @@
+#a
+vinit,imah=map(int,input().split())
+list1=list(map(int,input().split()))
+vinit=[]
+list1.insert(0,0)
+for e in range(imah):
+     din=[]
+     k=0
+     cc,dd=map(int,input().split())
+     for i in range(cc,dd+1):         
+         k^=list1[i]     
+     vinit.append(s)
+for e in vinit:
+     print(e)
