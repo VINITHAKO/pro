bd8826d99eb58ca87a114185bf78b121ee8f1388
VINITHAKO <51981011+VINITHAKO@users.noreply.github.com>
2019-08-01 13:51:16 +0530

    Update 18.py

--- 18.py
+++ 18.py
@@ -7,7 +7,7 @@ for k in range(gowri):
     for l in range(kur-1):
         for m in range(l+1,kur+1):
             if vin[k][l:m]==[1]*len(vin[k][l:m]):
-                 if all(vin[p+k][l:m]==[1]*len(vin[p+k][l:m]) for p in range(len(vin[k][l:m])-1)):
+                 if all(vin[s+k][l:m]==[1]*len(vin[s+k][l:m]) for s in range(len(vin[k][l:m])-1)):
                      if len(vin[k][l:m])>sel:
                         sel=len(vin[k][l:m])
 if len(vin)==1 and len(vin[0])==1 and vin[0][0]==1:
