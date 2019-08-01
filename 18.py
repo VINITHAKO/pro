699b0dd9d9a64d6436ef220e2d3db6b4b7c03e12
VINITHAKO <51981011+VINITHAKO@users.noreply.github.com>
2019-08-01 13:47:49 +0530

    Update 18.py

--- 18.py
+++ 18.py
@@ -1,16 +1,16 @@
 gowri,kur=map(int,input().split())
 vin=[]
 sel=0
-for i in range(gowri):
+for k in range(gowri):
     vin.append(list(map(int,input().split())))   
-for i in range(gowri):
-    for j in range(kur-1):
-        for k in range(j+1,kur+1):
-            if vin[i][j:k]==[1]*len(vin[i][j:k]):
-                 if all(vin[p+i][j:k]==[1]*len(vin[p+i][j:k]) for p in range(len(vin[i][j:k])-1)):
-                     if len(vin[i][j:k])>sel:
-                        sel=len(vin[i][j:k])
+for k in range(gowri):
+    for l in range(kur-1):
+        for m in range(l+1,kur+1):
+            if vin[k][l:m]==[1]*len(vin[k][l:m]):
+                 if all(vin[p+k][l:m]==[1]*len(vin[p+k][l:m]) for p in range(len(vin[k][l:m])-1)):
+                     if len(vin[k][l:m])>sel:
+                        sel=len(vin[k][l:m])
 if len(vin)==1 and len(vin[0])==1 and vin[0][0]==1:
     print(1)
-for i in range(sel):
+for k in range(sel):
     print(*[1]*sel)
