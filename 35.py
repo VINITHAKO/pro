b5645be8120226df20ca153a0b7a1fb80e77942b
VINITHAKO <51981011+VINITHAKO@users.noreply.github.com>
2019-08-03 14:10:46 +0530

    Create 35.py

---
+++ 35.py
@@ -0,0 +1,20 @@
+v1=input()
+s1=list(set(p1))
+y=1
+b=0
+check=False
+while True:
+    ke=p1[b]
+    for j in range(0,len(p1)-y):
+        if ke in p1[j:j+y]:
+            check=True
+        else:
+            check=False
+            b+=1
+            if b>=len(l1):
+              b=0
+              y+=1
+            break
+    if check==True:
+        break
+print(y)
