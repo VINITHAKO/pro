f443b85946931d2c7dc7af77d15d9614ffd2e014
VINITHAKO <51981011+VINITHAKO@users.noreply.github.com>
2019-08-03 14:05:59 +0530

    Update 34.py

--- 34.py
+++ 34.py
@@ -5,8 +5,8 @@ for i in range(m1):
       v.append(input())
 for i in range(m1):
       for j in range(n1-1):
-            if(v[i][j]!='S' and v[i][j+1]!='S'):
+            if(v[i][j]!='R' and v[i][j+1]!='R'):
                   cost+=3
-            elif(v[i][j]!='R' and v[i][j+1]!='R'):
+            elif(v[i][j]!='G' and v[i][j+1]!='G'):
                   cost+=5
 print(cost)
