c8f65ba912ef9325c2700be99d26337970088417
VINITHAKO <51981011+VINITHAKO@users.noreply.github.com>
2019-08-13 11:08:30 +0530

    Update 53.py

--- 53.py
+++ 53.py
@@ -1,6 +1,11 @@
-#a
-letters=list(input())
-if len(letters)>=26:
+v = input().split()
+v = list(set([*str("".join(n)).lower()]))
+s = 0
+a = "abcdefghijklmnopqrstuvxyz"
+for i in v:
+    if i in a:
+        s += 1
+if s == 25:
     print("yes")
 else:
     print("no")
